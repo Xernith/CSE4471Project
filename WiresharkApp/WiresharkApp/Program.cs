426c3589c4836e6e51a592845ed57bd755f9ef98
Patrick Pastore <patrick.j.pastore@gmail.com>
2017-11-12 23:22:43 -0500

    Basic program running tshark. Wireshark MUST be in path C:\Program Files\Wireshark\tshark.exe for program to work

--- WiresharkApp/WiresharkApp/Program.cs
+++ WiresharkApp/WiresharkApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WiresharkApp
@@ -10,6 +12,17 @@ namespace WiresharkApp
     {
         static void Main(string[] args)
         {
+            //Console.Out.WriteLine("Hello");
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            //IMPORTANT: make sure that the tshark exe is is the following path
+            //TODO: put a check to make sure it is in the path
+            startInfo.FileName = @"C:\Program Files\Wireshark\tshark.exe";
+            startInfo.Arguments = "-i 2";
+            process.StartInfo = startInfo;
+            process.Start();
         }
     }
 }
